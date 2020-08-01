0f61fca9145d7f2cbe6d3cb1b5ff0c580d320f47
Jordan Ngucho <jordan.ngucho@aims-cameroon.org>
2020-08-01 15:11:56 +0100

    Initialisation of the project: Scripts from the superinfection model to start from them.

---
+++ parameters_original.py
@@ -0,0 +1,47 @@
+# Author: Kristina B. Helle
+# parameter values that can be changed
+# version published in Whatsapp group on 2020-06-18
+name = "original"
+
+dummy = -1000000
+NE = NP = NI = NL = NM = 4  # 16
+N = 1000000000
+E1_0 = 0
+
+days = 365
+DE = 4  # 3.7
+DP = 1  # 1
+DI = 5  # 7
+DL = 5  # 7
+
+R0bar = 3  # ro = 1.2
+a = 0  # 0.5
+tROmax = 0  # 300
+
+cP = 0.5  #
+cI = 1  # 0.98
+cL = 0.5
+
+q = 0.5  # [value guessed]
+m_l = 0.05  # [value guessed]
+m_m = 0.1  # [value guessed]
+
+fsick_s = 0.82  # 0.1
+fsick_m = 0.9  # [value guessed]
+fdead_s = 0.003  # 0
+fdead_m = 0.006  # [value guessed]
+
+tiso1 = 0  # 105  # [value from riskgroups]
+tiso2 = 365  # 180  # [value from riskgroups]
+fiso_s = 0.2  # [value from a version of riskgroups]
+fiso_m = 0.3  # [value guessed]
+Qmax = 0
+phome = 0.75  # [value from a version of riskgroups]
+
+tdist1 = 1  #  [value made up] # 105  # [value from riskgroups]
+tdist2 = 165  # [value from riskgroups]
+pdist = 0.75  # [value from a version of riskgroups]
+
+LExt_s = 1  # LExt
+LExt_m = 0  # [value guessed]
+
